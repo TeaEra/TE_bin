241f9ffbe2a13f8865dbc8a934876351318dd8dd
CHEN Zhengong <cllf08214@qq.com>
2015-09-28 11:01:16 +0800

    te-scaffold;

---
+++ te-scaffold/README.md
@@ -0,0 +1,3 @@
+# te-scaffold
+
+> By teaera;
--- te-scaffold/index.js
+++ te-scaffold/index.js
@@ -6,30 +6,137 @@
 
 'use strict';
 
-console.log('>>> Welcome to teaera\'s scaffold! <<<');
-
 var
-    argv = process.argv,
-    userArgs = argv.slice(2);
+    welcomeStr = [
+    '',
+    ' ___________  ____________  ',
+    '/___    ___/  |  ________/  ',
+    '    |  |      |  |          ',
+    '    |  |      |  |_____     ',
+    '    |  |      |   ____/     ',
+    '    |  |      |  |          ',
+    '    |  |      |  |_______   ',
+    '    |__|      |__________\\  ',
+    '',
+    '>>> Welcome to teaera\'s scaffold! <<<',
+    ''
+];
+for (var i=0, size=welcomeStr.length; i<size; ++i) {
+    console.log(welcomeStr[i]);
+}
 
-// if (userArgs.length < 1) {
-//     console.log('> ERR: please add your project name as a paramter to tescf;');
-//     process.exit(1);
-// }
-
-// var
-//     projName = userArgs[0];
-
+/* Import */
 var
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
-    fse = require('fs.extra'),
-    ncp = require('ncp').ncp;
+    inquirer = require("inquirer"),
+    ncp = require('ncp').ncp,
+    mkdirp = require('mkdirp');
 
-ncp('node_modules/te-scaffold/app', './app', function (err) {
-    if (err) {
-        console.log('> ERR: ' + err);
-        process.exit(1);
+inquirer.prompt([
+    {
+        type: 'checkbox',
+        message: 'Init helper',
+        name: 'init',
+        choices: [
+            {
+                name: 'package.json',
+                checked: true
+            },
+            {
+                name: 'app',
+                checked: true
+            },
+            {
+                name: 'inc'
+            },
+            {
+                name: 'lib'
+            },
+            {
+                name: 'vendor'
+            },
+            {
+                name: '.editorconfig'
+            },
+        ]
     }
-    console.log('OK: ' + ' all inited!');
+], function (answers) {
+    var
+        components = answers.init;
+    for (var i=0, size=components.length; i<size; ++i) {
+        var
+            currComp = components[i];
+        switch (currComp) {
+            case 'package.json':
+                ncp('node_modules/te-scaffold/demo/' + currComp, './' + currComp, function (err) {
+                    if (err) {
+                        console.log('> ERR: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('OK: ' + currComp + ' inited!');
+                });
+                break;
+            case 'app':
+                ncp('node_modules/te-scaffold/demo/' + currComp, './' + currComp, function (err) {
+                    if (err) {
+                        console.log('> ERR: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('OK: ' + currComp + ' inited!');
+                });
+                // TODO:
+                mkdirp.sync('./' + currComp + '/api');
+                mkdirp.sync('./' + currComp + '/conf');
+                mkdirp.sync('./' + currComp + '/static/css');
+                mkdirp.sync('./' + currComp + '/static/js');
+                mkdirp.sync('./' + currComp + '/static/images');
+                mkdirp.sync('./' + currComp + '/static/lib');
+                break;
+            case 'inc':
+                ncp('node_modules/te-scaffold/demo/optional/' + currComp, './app/' + currComp, function (err) {
+                    if (err) {
+                        console.log('> ERR: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('OK: ' + currComp + ' inited!');
+                });
+                break;
+            case 'lib':
+                ncp('node_modules/te-scaffold/demo/optional/' + currComp, './app/' + currComp, function (err) {
+                    if (err) {
+                        console.log('> ERR: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('OK: ' + currComp + ' inited!');
+                });
+                break;
+            case 'vendor':
+                // ncp('node_modules/te-scaffold/demo/optional/' + currComp, './app/' + currComp, function (err) {
+                //     if (err) {
+                //         console.log('> ERR: ' + err);
+                //         process.exit(1);
+                //     }
+                //     console.log('OK: ' + currComp + ' inited!');
+                // });
+                // TODO:
+                mkdirp.sync('./app/' + currComp);
+                break;
+            case '.editorconfig':
+                ncp('node_modules/te-scaffold/demo/' + currComp.replace('.', '_'), './' + currComp, function (err) {
+                    if (err) {
+                        console.log('> ERR: ' + err);
+                        process.exit(1);
+                    }
+                    console.log('OK: ' + currComp + ' inited!');
+                });
+                break;
+            default:
+                // Pass;
+        }
+    }
 });
+
+
+/**************************************************/
+// var
+//     argv = process.argv,
+//     userArgs = argv.slice(2);
